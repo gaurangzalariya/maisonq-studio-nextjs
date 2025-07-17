1bf25d76a4109c51b3909a103c2218c0f69d833f
Gaurang Zalariya <zgppatel@gmail.com>
2025-07-17 17:46:53 +0530

    Integrating GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import dynamic from 'next/dynamic';
 
 import Header from "@/components/Header";
 import Container from "@/components/Container";
+import { GoogleTagManagerScript, GoogleTagManagerNoScript } from "@/components/GoogleTagManager";
 
 // Lazy load non-critical components
 const Footer = dynamic(() => import("@/components/Footer"), {
@@ -55,6 +56,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={`${inter.variable} ${inter.className}`}>
       <head>
+        <GoogleTagManagerScript gtmId="GTM-55GQ7JZL" />
         {/* Preconnect to external domains */}
         <link
           rel="preconnect"
@@ -104,26 +106,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <noscript>
           <link rel="stylesheet" href="https://use.typekit.net/mct8xox.css" />
         </noscript>
-
-        {/* Google Tag Manager */}
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-55GQ7JZL');</script>
-        {/* End Google Tag Manager */}
       </head>
       <body className="font-primary text-gray-900 bg-[#f8f8f8] antialiased">
+        <GoogleTagManagerNoScript gtmId="GTM-55GQ7JZL" />
         <Container>
           <Header />
           <main>{children}</main>
           <Footer />
         </Container>
-
-        {/* Google Tag Manager (noscript) */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-55GQ7JZL"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        {/* End Google Tag Manager (noscript) */}
       </body>
     </html>
   )
