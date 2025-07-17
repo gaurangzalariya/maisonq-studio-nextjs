ca3831d5ee3e9849258dbde24af91c0dc6ad292b
Gaurang Zalariya <zgppatel@gmail.com>
2025-07-17 17:43:07 +0530

    Integrating GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import dynamic from 'next/dynamic';
 
 import Header from "@/components/Header";
 import Container from "@/components/Container";
-import ClientProviders from "@/components/ClientProviders";
 
 // Lazy load non-critical components
 const Footer = dynamic(() => import("@/components/Footer"), {
@@ -105,6 +104,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <noscript>
           <link rel="stylesheet" href="https://use.typekit.net/mct8xox.css" />
         </noscript>
+
+        {/* Google Tag Manager */}
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-55GQ7JZL');</script>
+        {/* End Google Tag Manager */}
       </head>
       <body className="font-primary text-gray-900 bg-[#f8f8f8] antialiased">
         <Container>
@@ -112,7 +119,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <main>{children}</main>
           <Footer />
         </Container>
-        <ClientProviders />
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-55GQ7JZL"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
       </body>
     </html>
   )
